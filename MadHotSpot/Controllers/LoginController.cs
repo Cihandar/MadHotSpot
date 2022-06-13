756170d8b888e6e128b5ce3e85751b73351aca7e
Ali Zeki KILIÇ <ali@goktas.net>
2022-06-13 20:54:51 +0300

    cccc

--- MadHotSpot/Controllers/LoginController.cs
+++ MadHotSpot/Controllers/LoginController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using MadHotSpot.Models;
 using Microsoft.AspNetCore.Identity;
 using tik4net;
+using MadHotSpot.Models.ViewModel;
+using tik4net.Objects;
 
 namespace MadHotSpot.Controllers
 {
@@ -31,6 +33,48 @@ namespace MadHotSpot.Controllers
             var data = context.H_HotSpotAyar.FirstOrDefault(x => x.FirmaId == FirmaId);
 
             return View(data); 
+        }
+
+        [HttpPost("LoginCheck")]
+        public async Task<bool> LoginCheck(CustomerInfoViewModel customer)
+        {
+            var ayar = context.H_Ayarlar.FirstOrDefault(x => x.FirmaId == customer.FirmaId);
+
+
+            if (ayar == null) return false;
+
+            try
+            {
+                using (var conn = ConnectionFactory.OpenConnection(TikConnectionType.Api_v2, ayar.MikrotikIp, int.Parse(ayar.MikrotikPort), ayar.MikrotikUser, ayar.MikrotikPass))
+                {
+                    var user = conn.LoadList<tik4net.Objects.Ip.Hotspot.HotspotUser>().Where(x => x.Name == customer.BirthDate && x.Password == customer.RoomNumber).FirstOrDefault();
+
+                    if (user != null)
+                    {
+
+                        context.H_CustomerInfo.Add(new CustomerInfo
+                        {
+
+                            PhoneNumber = customer.PhoneNumber,
+                            Email = customer.Email,
+                            FirmaId = customer.FirmaId,
+                            BirthDate = customer.BirthDate
+
+                        });
+                        context.SaveChanges();
+                        return true;
+
+
+                    }
+                    else return false;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
         }
     }
 }
